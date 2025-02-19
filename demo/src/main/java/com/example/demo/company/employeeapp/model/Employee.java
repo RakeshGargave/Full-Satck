5fe3530f6544cb08aa94c02402626c59ab814632
Puja <pm4419070@gmail.com>
2025-02-19 11:54:24 +0530

    added space

--- demo/src/main/java/com/example/demo/company/employeeapp/model/Employee.java
+++ demo/src/main/java/com/example/demo/company/employeeapp/model/Employee.java
@@ -5,7 +5,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "employeeee")   // Matches exact table name
-public class Employee {
+public class Employee{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
